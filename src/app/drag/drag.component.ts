5719bdc1ccdbfebd49dcd9a121d1c9e367881586
sudipto roy <sudipto.roy.1985@gmail.com>
2018-12-01 14:32:57 +0530

    dynamic draggable items

--- src/app/drag/drag.component.ts
+++ src/app/drag/drag.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
+  
+
+
+
 @Component({
   selector: 'app-drag',
   templateUrl: './drag.component.html',
@@ -7,10 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DragComponent implements OnInit {
 
+  count: number = 10;
+
+  items: item[] = [];
+
   constructor() { }
 
   ngOnInit() {
-    console.log("init")
+    this.items.push({name:"first",id:1})
+    this.items.push({name:"second",id:2})
+    this.items.push({name:"third",id:3})
+    this.items.push({name:"fourth",id:4})
+    this.items.push({name:"fifth",id:5})
+    this.items.push({name:"sixth",id:6})
+
   }
 
   allowDrop(ev) {
@@ -30,4 +45,9 @@ export class DragComponent implements OnInit {
     el.appendChild(document.getElementById(data));
   }
 
+}
+
+interface item{
+  name: string,
+  id: number,
 }
